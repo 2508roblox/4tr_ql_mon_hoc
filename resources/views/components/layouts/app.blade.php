08323a9f9e7ec1a6716b2f2e4b3af3f9115a3cb6
2509ROBLOX <111523395+hg-digital-company-limited@users.noreply.github.com>
2025-03-21 20:42:10 +0700

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
- 
+ <meta name="google-site-verification" content="zYfwKdmrCEkdvAZnSCwDApHZaSOeumxORzrX9IO4fjk" />
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
